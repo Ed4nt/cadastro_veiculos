ff9718173f4fff1dfb8cf483be6116b68881ea51
Edelson Bessa <edelsonbessa@gmail.com>
2026-03-20 18:43:23 -0300

    criando classes para armazenar veiculos

--- bin/cadastro_veiculos.dart
+++ bin/cadastro_veiculos.dart
@@ -13,18 +13,113 @@ Mini-desafio:
 Crie um programa Dart que:
 Armazene 2 veículos usando Map
 Guarde tudo em uma List
-Imprima no console */
+Imprima no console 
 
-Map carro1 = {'Marca': 'Toyota', 'Modelo': 'Corolla', 'Ano': 2011, 'Placa': 'ABC1D23'};
-Map carro2 = {'Marca': 'Chevrolet', 'Modelo': 'S10', 'Ano': 2020, 'Placa': 'EFG4H56'};
+void main() {
+  Map<String, dynamic> carro1 = {'Marca': 'Toyota', 'Modelo': 'Corolla', 'Ano': 2011, 'Placa': 'ABC1D23'};
+  Map<String, dynamic> carro2 = {'Marca': 'Chevrolet', 'Modelo': 'S10', 'Ano': 2020, 'Placa': 'EFG4H56'};
+
+  List<Map<String, dynamic>> carros = [carro1, carro2];
 
 
+  for(int i=0; carros.length > i; i++) {
+      print('''
+      Marca: ${carros[i]['Marca']}
+      Modelo: ${carros[i]['Modelo']}
+      Ano: ${carros[i]['Ano']}
+      Placa: ${carros[i]['Placa']}
+      ''');
+  }
+}
 
-List<Map> carros = [carro1, carro2];
+
+2. Controle de Fluxo
+Conceito:
+if, else
+switch
+for, while
+
+Por que isso importa?
+Você vai validar dados:
+Placa vazia?
+Ano válido?
+
+Mini-desafio:
+Peça um ano e verifique:
+Se é maior que 1886 (primeiro carro da história)
+Se for inválido, exiba erro
+
 
 
 void main() {
-  for(int i=0; i<carros.length; i++) {
-    print(carros[i]);
+ 
+  
+  int ano = 1886;
+
+  if(ano >= 1886) {
+    print('Ano válido!');
+  } else {
+    print('Ano inválido!');
+  }
+ 
+}
+
+
+
+bool validarPlaca(String placa) {
+  return placa.length == 7;
+}
+
+void main() {
+  String placaVeiculo = 'ABC1D23';
+  if (validarPlaca(placaVeiculo)) {
+    print('Placa válida');
+  } else {
+    print('Placa inválida');
   }
 }
+
+Mini-desafio:
+Crie uma classe Veiculo com:
+ano
+modelo
+marca
+placa
+
+E um método:
+void exibirDados()
+
+*/
+
+class Veiculo {
+  String marca;
+  String modelo;
+  String placa;
+  int ano;
+
+  Veiculo({
+    required this.marca,
+    required this.modelo,
+    required this.placa,
+    required this.ano,
+  });
+
+  void exibirDados() {
+    print('''
+    Marca: $marca
+    Modelo: $modelo
+    Placa: $placa
+    Ano: $ano
+    ''');
+  }
+
+}
+
+void main() {
+  Veiculo carro = Veiculo(marca: 'Toyota', modelo: 'Corolla', placa: 'ABC1D23', ano: 2011);
+  carro.exibirDados();
+
+  Veiculo carro2 = Veiculo(marca: 'Chevrolet', modelo: 'S10', placa: 'EFG4H56', ano: 2020);
+  carro2.exibirDados();
+}
+
